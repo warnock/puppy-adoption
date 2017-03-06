6fb76b32e5a779354922163f2929da27316b95af
Chris Carr and Gloria Friesen <gloria@>
2017-03-06 10:12:58 -0800

    add exclamation mark

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var i = 1;                     //  set your counter to 1
 function myLoop () {           //  create a loop function
    setTimeout(function () {
       var day = i;                          //  call a 3s setTimeout when the loop is called
-      console.log("Day: " + i);
+      console.log("Day: " + i "!");
       var rollNumber = Math.random()*(100 - 1) + 1;
       if (rollNumber < 15) {
         alert("DISEASE");
