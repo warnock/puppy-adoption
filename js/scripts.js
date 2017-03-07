deabf80d339dce66091879fa843ad887e83c031c
Ryan McAlpin and Riley Watts <riley@>
2017-03-06 16:13:39 -0800

    something else

--- js/scripts.js
+++ js/scripts.js
@@ -82,4 +82,6 @@ var travel = function() {
 var char1 = new Character("Ryan");
 var char2 = new Character("Riley");
 var char3 = new Character("Chris");
-caravan.party.push(char1, char2, char3);
+var char4 = new Character("Gloria");
+var char5 = new Character("Megan");
+caravan.party.push(char1, char2, char3, char4, char5);
